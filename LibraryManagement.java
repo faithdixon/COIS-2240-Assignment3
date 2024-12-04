cff4916ab7bc61d42202235361166739f47046ae
faithdixon <faithmedixon@gmail.com>
2024-12-03 21:05:28 -0500

    Singleton Design Pattern
    The Singleton design pattern was added to the Transaction class and the static keyword was removed from method headers. The LibraryManagement class now uses the Singleton instance of Transaction

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class LibraryManagement {
     private Library library = new Library();
+    //Singleton
+    private Transaction transaction = Transaction.getInstance();
 
     public static void main(String[] args) {
         new LibraryManagement().run();
@@ -76,7 +78,7 @@ public class LibraryManagement {
                     Book book = library.findBookById(bookId);
 
                     if (member != null && book != null) {
-                    	Transaction.borrowBook(book, member);
+                    	transaction.borrowBook(book, member);
                     } else {
                         System.out.println("Invalid member or book ID.");
                     }
@@ -94,7 +96,7 @@ public class LibraryManagement {
                     book = library.findBookById(bookId);
 
                     if (member != null && book != null) {
-                    	Transaction.returnBook(book, member);
+                    	transaction.returnBook(book, member);
                     } else {
                         System.out.println("Invalid member or book ID.");
                     }
@@ -116,7 +118,7 @@ public class LibraryManagement {
                     }
                     break;
                 case 6:
-                	Transaction.displayTransactionHistory();
+                	transaction.displayTransactionHistory();
                     break;
                 case 7:
                     System.out.println("Exiting. Good Bye..");
